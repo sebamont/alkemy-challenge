1cfdd07f987c3e81ce63b4edbe10381ce6235e42
Sebastian Montagna <sebamontagna@gmail.com>
2021-03-27 15:36:06 -0300

    changes for heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 //Colors lib help us read faster the console.
@@ -21,6 +22,12 @@ app.use(express.json());
 
 
 app.use('/api/movements',movements);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html')))
+}
 
 
 const PORT = process.env.PORT ;
